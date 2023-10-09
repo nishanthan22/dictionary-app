9bb5ace75d1bacbc6a1b3c3a1c5c8f7c6be5ce85
Nishanthan Ravichandran <nishanthravi226@gmail.com>
2023-10-09 13:23:18 -0400

    develop branch created and code setup completed

---
+++ src/com/acc/a1/dictionary/Dictionary.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.acc.a1.dictionary;
+
+/**
+ * 
+ */
+public class Dictionary {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The project setup is completed");
+	}
+
+}
